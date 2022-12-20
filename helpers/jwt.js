e22f629d3cbf4b0885eb9933b0054690fa01b02d
Andrey <andrey.amrulloh@koinworks.com>
2022-12-20 16:45:58 +0700

    jest unit test

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,17 +1,18 @@
-const jwt = require('jsonwebtoken')
-const SECRET_KEY = 'rahasia'
+const jwt = require('jsonwebtoken');
+
+const SECRET_KEY = 'rahasia';
 
 const generateToken = (payload) => {
-  const token = jwt.sign(payload, SECRET_KEY)
-  return token
-}
+  const token = jwt.sign(payload, SECRET_KEY);
+  return token;
+};
 
 const verifyToken = (token) => {
-  const decoded = jwt.verify(token, SECRET_KEY)
-  return decoded
-}
+  const decoded = jwt.verify(token, SECRET_KEY);
+  return decoded;
+};
 
 module.exports = {
   generateToken,
-  verifyToken
-}
+  verifyToken,
+};
--- tests/routes.test.js
+++ tests/routes.test.js
@@ -1,10 +1,21 @@
-const request = require('supertest')
-const app = require('../app')
+const request = require('supertest');
+const app = require('../app');
+const { hashPassword, comparePassword } = require('../helpers/bcrypt');
 
-describe('Sample Test', () => {
-  it('should fetch all albums', async () => {
+describe('User Test', () => {
+  const password = 'admin'; // datainput
+  const hashing = hashPassword(password);
+  const compare = comparePassword(password, hashing);
+  it('Password compare to be return boolean', () => { // unit testing
+    expect(typeof compare).toBe('boolean');
+  });
+  it('Should return true when compile password', () => {
+    expect(compare).toEqual(true);
+  });
+
+  it('should fetch all albums', async () => { // integration testing
     const res = await request(app).get('/photos');
     expect(res.statusCode).toEqual(200);
     // expect(res.body).toHaveProperty('post');
   });
-})
+});
