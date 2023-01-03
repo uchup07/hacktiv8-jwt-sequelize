1b84ac4260b97c96d1dfa013a0b6d2676322260b
Yusuf <uchup_bell@yahoo.com>
2023-01-03 17:40:33 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const router = require('./routers');
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
